aa259dbda5b2adc801abc281996844701529abe5
smnam-laptop <sumannam@gmail.com>
2025-03-24 14:28:59 +0900

    4주차 원의 면적 계산하는 프로그램 개발 완료

---
+++ HelloWorld1/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // 1. 실수형 변수(round)를 선언한다.
+        // 2. 파이(PI)를 상수로 선언한다.
+        // 3. 결과 = round * round * PI
+        // 4. '결과'를 출력한다.
+    }
+}
